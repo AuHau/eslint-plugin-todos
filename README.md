f21b34d2d757e72b9d0f64b2124fcab0bff1c9c8
Adam Uhlíř <adam@uhlir.dev>
2020-03-30 16:00:07 +0200

    feat: detect undocumented todos

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# eslint-plugin-output-todo-comments [![npm version](https://badge.fury.io/js/eslint-plugin-output-todo-comments.svg)](https://badge.fury.io/js/eslint-plugin-output-todo-comments)
-Contains single rule that warns about used warning comments and shows them as they are.
+# eslint-plugin-todos [![npm version](https://badge.fury.io/js/eslint-plugin-todos.svg)](https://badge.fury.io/js/eslint-plugin-todos)
 
-This plugin contains stupidly copy-pasted **no-warning-comments** rule with one little modification: it shows warn comment as it is instead of showing meaningless warning "Unexpected 'todo' comment". [Check out original documentation for no-warning-comments](http://eslint.org/docs/rules/no-warning-comments).
+Contains single rule that detects TODO (or similar) comments which are not documented inside issue tracker (eq. have a link to an existing issue).
+
+The link to the issue tracker can be either configured using `url` property of the rule's configuration, or is
+automatically detected from the `package.json` using either `bugs` or `repository` properties. 
+
+This plugin is based on the **no-warning-comments** rule. [Check out original documentation for no-warning-comments](http://eslint.org/docs/rules/no-warning-comments).
+
+#### Fails
 
 ```js
 // TODO: We may need to make this thing better
@@ -9,26 +15,34 @@ This plugin contains stupidly copy-pasted **no-warning-comments** rule with one 
 // TODO: Wow!
 ```
 
-![](http://i.imgur.com/Od8jwmX.png)
+#### Passes
 
-The plugin doesn't include any tests and it's created as quick and dirty solution for my internal needs.
+```js
+// TODO: Needs refactor https://github.com/auhau/eslint-plugin-todos/issues/4
+// TODO: Waiting for https://github.com/auhau/eslint-plugin-todos/issues/2
+```
 
 ## Install
 ```
-npm install -D eslint-plugin-output-todo-comments
+npm install -D eslint-plugin-todos
 ```
 
 ## Usage
 ```js
 module.exports = {
-    "plugins": ["output-todo-comments"],
+    "plugins": ["todos"],
     "rules": {
-        'output-todo-comments/output-todo-comments': [
+        'todos/only-documented-todos': [
             'warn', {
                 'terms': ['todo'],
                 'location': 'start',
+                'url': 'https://github.com/auhau/eslint-plugin-todos'
             }
         ],
     }
 }
 ```
+
+## Credit
+
+This rule was extended using [eslint-plugin-output-todo-comments](https://github.com/finom/eslint-plugin-output-todo-comments) by [finom](https://github.com/finom)
--- index.js
+++ index.js
@@ -1,133 +1,214 @@
 /**
- * @fileoverview Rule that warns about used warning comments and shows they as are
+ * @fileoverview Rule that warns about used warning comments
  * @author Alexander Schmidt <https://github.com/lxanders>
- * @author Andrey Gubanov <https://github.com/lxanders>
  */
 
-"use strict";
+'use strict'
 
-var ellipsize = require('ellipsize');
+const { escapeRegExp } = require('lodash')
+const readPkgUp = require('read-pkg-up')
+const ellipsize = require('ellipsize');
+
+function isDirectiveComment (node) {
+  const comment = node.value.trim()
+
+  return (
+    node.type === 'Line' && comment.indexOf('eslint-') === 0 ||
+    node.type === 'Block' && (
+      comment.indexOf('global ') === 0 ||
+      comment.indexOf('eslint ') === 0 ||
+      comment.indexOf('eslint-') === 0
+    )
+  )
+}
+
+function getRepoUrl () {
+  const packageJson = readPkgUp.sync().packageJson
+
+  if (packageJson.bugs) {
+    if (typeof packageJson.bugs === 'string') {
+      return packageJson.bugs
+    }
+
+    if (packageJson.bugs.url) {
+      return packageJson.bugs.url
+    }
+  }
+
+  if (packageJson.repository) {
+    if (typeof packageJson.repository === 'string') {
+      return packageJson.repository
+    }
+
+    if (packageJson.resolutions.url) {
+      return packageJson.repository.url
+    }
+  }
+
+  return undefined
+}
 
 //------------------------------------------------------------------------------
 // Rule Definition
 //------------------------------------------------------------------------------
 
-var ruleDef = {
-    meta: {
-        docs: {
-            description: "warn about todos",
-            category: "Best Practices",
-            recommended: false
-        },
+const ruleDef = {
+  meta: {
+    type: 'suggestion',
 
-        schema: [
-            {
-                type: "object",
-                properties: {
-                    terms: {
-                        type: "array",
-                        items: {
-                            type: "string"
-                        }
-                    },
-                    location: {
-                        enum: ["start", "anywhere"]
-                    }
-                },
-                additionalProperties: false
-            }
-        ]
+    docs: {
+      description: 'disallow specified warning terms in comments without proper documentation',
+      category: 'Best Practices',
+      recommended: false
     },
 
-    create: function(context) {
-
-        var configuration = context.options[0] || {},
-            warningTerms = configuration.terms || ["todo", "fixme", "xxx"],
-            location = configuration.location || "start",
-            selfConfigRegEx = /\bno-warning-comments\b/,
-            warningRegExps;
-
-        /**
-         * Convert a warning term into a RegExp which will match a comment containing that whole word in the specified
-         * location ("start" or "anywhere"). If the term starts or ends with non word characters, then the match will not
-         * require word boundaries on that side.
-         *
-         * @param {string} term A term to convert to a RegExp
-         * @returns {RegExp} The term converted to a RegExp
-         */
-        function convertToRegExp(term) {
-            var escaped = term.replace(/[-\/\\$\^*+?.()|\[\]{}]/g, "\\$&"),
-                suffix,
-                prefix;
-
-            /*
-             * If the term ends in a word character (a-z0-9_), ensure a word
-             * boundary at the end, so that substrings do not get falsely
-             * matched. eg "todo" in a string such as "mastodon".
-             * If the term ends in a non-word character, then \b won't match on
-             * the boundary to the next non-word character, which would likely
-             * be a space. For example `/\bFIX!\b/.test('FIX! blah') === false`.
-             * In these cases, use no bounding match. Same applies for the
-             * prefix, handled below.
-             */
-            suffix = /\w$/.test(term) ? "\\b" : "";
-
-            if (location === "start") {
-
-                /*
-                 * When matching at the start, ignore leading whitespace, and
-                 * there's no need to worry about word boundaries.
-                 */
-                prefix = "^\\s*";
-            } else if (/^\w/.test(term)) {
-                prefix = "\\b";
-            } else {
-                prefix = "";
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          terms: {
+            type: 'array',
+            items: {
+              type: 'string'
             }
+          },
+          location: {
+            enum: ['start', 'anywhere']
+          },
+          url: {
+            type: 'string'
+          }
+        },
+        additionalProperties: false
+      }
+    ]
+  },
 
-            return new RegExp(prefix + escaped + suffix, "i");
-        }
+  create (context) {
 
-        /**
-         * Checks the specified comment for matches of the configured warning terms and returns the matches.
-         * @param {string} comment The comment which is checked.
-         * @returns {Array} All matched warning terms for this comment.
+    const sourceCode = context.getSourceCode(),
+      configuration = context.options[0] || {},
+      warningTerms = configuration.terms || ['todo', 'fixme', 'xxx'],
+      location = configuration.location || 'start',
+      selfConfigRegEx = /\bno-warning-comments\b/u,
+      repoUrl = configuration.url || getRepoUrl(),
+      repoUrlRegEx = repoUrl ? new RegExp(`${escapeRegExp(repoUrl.trim())}`, 'iu') : undefined
+
+    /**
+     * Convert a warning term into a RegExp which will match a comment containing that whole word in the specified
+     * location ("start" or "anywhere"). If the term starts or ends with non word characters, then the match will not
+     * require word boundaries on that side.
+     * @param {string} term A term to convert to a RegExp
+     * @returns {RegExp} The term converted to a RegExp
+     */
+    function convertToRegExp (term) {
+      const escaped = escapeRegExp(term)
+      const wordBoundary = '\\b'
+      const eitherOrWordBoundary = `|${wordBoundary}`
+      let prefix
+
+      /*
+       * If the term ends in a word character (a-z0-9_), ensure a word
+       * boundary at the end, so that substrings do not get falsely
+       * matched. eg "todo" in a string such as "mastodon".
+       * If the term ends in a non-word character, then \b won't match on
+       * the boundary to the next non-word character, which would likely
+       * be a space. For example `/\bFIX!\b/.test('FIX! blah') === false`.
+       * In these cases, use no bounding match. Same applies for the
+       * prefix, handled below.
+       */
+      const suffix = (/\w$/u.test(term) ? '\\b' : '') + ':?'
+
+      if (location === 'start') {
+
+        /*
+         * When matching at the start, ignore leading whitespace, and
+         * there's no need to worry about word boundaries.
          */
-        function commentContainsWarningTerm(comment) {
-            var matches = [];
+        prefix = '^\\s*'
+      } else if (/^\w/u.test(term)) {
+        prefix = wordBoundary
+      } else {
+        prefix = ''
+      }
 
-            warningRegExps.forEach(function(regex, index) {
-                if (regex.test(comment)) {
-                    matches.push(warningTerms[index]);
-                }
-            });
+      if (location === 'start') {
 
-            return matches;
-        }
-
-        /**
-         * Checks the specified node for matching warning comments and reports them.
-         * @param {ASTNode} node The AST node being checked.
-         * @returns {void} undefined.
+        /*
+         * For location "start" the regex should be
+         * ^\s*TERM\b.  This checks the word boundary
+         * at the beginning of the comment.
          */
-        function checkComment(node) {
-            var matches = commentContainsWarningTerm(node.value);
+        return new RegExp(prefix + escaped + suffix, 'iu')
+      }
 
-            matches.forEach(function(matchedTerm) {
-                context.report(node, ellipsize(node.value.trim(), 60));
-            });
-        }
-
-        warningRegExps = warningTerms.map(convertToRegExp);
-        return {
-            BlockComment: checkComment,
-            LineComment: checkComment
-        };
+      /*
+       * For location "anywhere" the regex should be
+       * \bTERM\b|\bTERM\b, this checks the entire comment
+       * for the term.
+       */
+      return new RegExp(prefix + escaped + suffix + eitherOrWordBoundary + term + wordBoundary, 'iu')
     }
-};
+
+    const warningRegExps = warningTerms.map(convertToRegExp)
+
+    /**
+     * Checks the specified comment for matches of the configured warning terms and returns the matches.
+     * @param {string} comment The comment which is checked.
+     * @returns {boolean} If comment has unwanted term.
+     */
+    function getUndocumentedTodoMessage (comment) {
+      for (const regex of warningRegExps) {
+        if (regex.test(comment) && !commentHasIssue(comment)) {
+          const fullMessage = comment.replace(regex, '')
+          return ellipsize(fullMessage.trim(), 60)
+        }
+      }
+
+      return false
+    }
+
+    function commentHasIssue (comment) {
+      if (!repoUrlRegEx) {
+        return false
+      }
+
+      return repoUrlRegEx.test(comment)
+    }
+
+    /**
+     * Checks the specified node for matching warning comments and reports them.
+     * @param {ASTNode} node The AST node being checked.
+     * @returns {void} undefined.
+     */
+    function checkComment (node) {
+      if (isDirectiveComment(node) && selfConfigRegEx.test(node.value)) {
+        return
+      }
+
+      const todoMessage = getUndocumentedTodoMessage(node.value)
+      if (todoMessage) {
+        context.report({
+          node,
+          message: `Undocumented TODO: ${todoMessage}`
+        })
+      }
+    }
+
+    return {
+      Program () {
+        const comments = sourceCode.getAllComments()
+
+        comments
+          .filter(token => token.type !== 'Shebang')
+          .forEach(checkComment)
+      }
+    }
+  }
+}
 
 module.exports = {
-    rules: {
-        'output-todo-comments': ruleDef
-    }
+  rules: {
+    'only-documented-todos': ruleDef
+  }
 }
